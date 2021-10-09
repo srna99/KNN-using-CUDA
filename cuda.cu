e3e79b4a45daa23a6707f5c95c920997abee958f
Serena Cheng <serena.ch1999@gmail.com>
2021-10-09 17:29:09 -0400

    Finish cuda and kernel work

--- cuda.cu
+++ cuda.cu
@@ -17,50 +17,56 @@
 
 using namespace std;
 
-__device__ float distance(float *testStart, float *testEnd, float *trainStart, float *trainEnd) {
-//     float sum = 0;
+__device__ float distance(float *a, float *b, int size) {
+    float sum = 0;
 
-//     for (int i = 0; i < a->size() - 1; i++) {
-//         float diff = (a->get(i)->operator float() - b->get(i)->operator float());
-//         sum += diff * diff;
-//     }
-
-//     return sum;
-}
-
-__global__ void KNN(float *train, float *test, int *predictions, int k, int numAttr, int numClasses, int *datasetSizes) {
-    // Implements a sequential kNN where for each candidate query an in-place priority queue is maintained to identify the kNN's.
-
-    // stores k-NN candidates for a query vector as a sorted 2d array. First element is inner product, second is class.
-    float *candidates = (float *) calloc(k * 2, sizeof(float));
-    for (int i = 0; i < 2 * k; i++) {
-        candidates[i] = FLT_MAX;
+    for(int i = 0; i < size - 1; i++) {
+        float diff = *(a + i) - *(b + i);
+        // printf("[%d, %d] %f, %f, %f\n", i, size, *(a + i),*(b + i), diff);
+        sum += diff * diff;
     }
 
-    // Stores bincounts of each class over the final set of candidate NN
-    int *classCounts = (int *) calloc(numClasses, sizeof(int));
+    return sum;
+}
 
-    // printf(*test + 0, *test + 0 + 8, test[0], test[8]);
+__global__ void KNN(float *train, float *test, int *predictions, float *candidates, int *classCounts, int *datasetSizes , int k, int numAttr, int numClasses) {
+    // Implements a sequential kNN where for each candidate query an in-place priority queue is maintained to identify the kNN's.
 
-    for (int queryIndex = 0; queryIndex < datasetSizes[1]; queryIndex++) {
-        for (int keyIndex = 0; keyIndex < datasetSizes[0]; keyIndex++) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
 
-            // float dist = distance(test->get_instance(queryIndex), train->get_instance(keyIndex));
-            float dist = distance(test + queryIndex, test + queryIndex + numAttr, train + keyIndex, train + keyIndex + numAttr);
+    if(tid < datasetSizes[1]) {
+        // printf("(%d)\n", tid);
+        // printf("(%f, %f), (%f, %f)\n", test[0], train[0], test[1], train[1]);
+        // printf("%d", predictions[0]);
+        // printf("%d, %d", datasetSizes[0], datasetSizes[1]);
+        // printf("{%d, %d}\n", numAttr, numClasses);
+        // printf("[%f, %d]\n", candidates[0], classCounts[0]);
+
+        // int queryIndex = tid * numAttr;
+        int candidateForTid = tid * 2 * k;
+
+        for (int keyIndex = 0; keyIndex < datasetSizes[0] * numAttr; keyIndex += numAttr) {
+            float dist = distance(test + tid * numAttr, train + keyIndex, numAttr);
+
+            // printf("%d: %f\n", tid, dist);
 
             // Add to our candidates
             for (int c = 0; c < k; c++) {
-                if (dist < candidates[2 * c]) {
+                if (dist < candidates[candidateForTid + 2 * c]) {
                     // Found a new candidate
                     // Shift previous candidates down by one
                     for (int x = k - 2; x >= c; x--) {
-                        candidates[2 * x + 2] = candidates[2 * x];
-                        candidates[2 * x + 3] = candidates[2 * x + 1];
+                        candidates[candidateForTid + 2 * x + 2] = candidates[candidateForTid + 2 * x];
+                        candidates[candidateForTid + 2 * x + 3] = candidates[candidateForTid + 2 * x + 1];
                     }
 
                     // Set key vector as potential k NN
-                    candidates[2 * c] = dist;
-                    candidates[2 * c + 1] = train->get_instance(keyIndex)->get(train->num_attributes() - 1)->operator float(); // class value
+                    candidates[candidateForTid + 2 * c] = dist;
+                    // candidates[2 * c + 1] = train->get_instance(keyIndex)->get(train->num_attributes() - 1)->operator float(); // class value
+                    candidates[candidateForTid + 2 * c + 1] = train[keyIndex + numAttr - 1];
+
+                    // printf("%d, %d: %f, %f\n", tid, keyIndex/8, candidates[candidateForTid + 2 * c], candidates[candidateForTid + 2 * c + 1]);
+                    // printf("%d, %d: %f, %f\n", tid, keyIndex/8, train[keyIndex], train[keyIndex + numAttr - 1]);
 
                     break;
                 }
@@ -68,32 +74,33 @@ __global__ void KNN(float *train, float *test, int *predictions, int k, int numA
         }
 
         // Bincount the candidate labels and pick the most common
+        int classOfCandidate;
+
         for (int i = 0; i < k; i++) {
-            classCounts[(int)candidates[2 * i + 1]] += 1;
+            classOfCandidate = (int) candidates[candidateForTid + 2 * i + 1];
+            // printf("%d: %d\n", tid, classOfCandidate);
+            classCounts[classOfCandidate + tid * numClasses] += 1;
         }
 
         int max = -1;
         int max_index = 0;
-        for (int i = 0; i < num_classes; i++) {
-            if (classCounts[i] > max) {
-                max = classCounts[i];
+        for (int i = 0; i < numClasses; i++) {
+            if (classCounts[i + tid * numClasses] > max) {
+                max = classCounts[i + tid * numClasses];
                 max_index = i;
             }
         }
 
-        predictions[queryIndex] = max_index;
+        predictions[tid] = max_index;
 
-        for (int i = 0; i < 2 * k; i++) {
-            candidates[i] = FLT_MAX;
-        }
-        memset(classCounts, 0, num_classes * sizeof(int));
+        // printf("%d: %d\n", tid, predictions[tid]);
     }
 }
 
 int *computeConfusionMatrix(int *predictions, ArffData *dataset) {
     int *confusionMatrix = (int *) calloc(dataset->num_classes() * dataset->num_classes(), sizeof(int)); // matrix size numberClasses x numberClasses
 
-    for (int i = 0; i < dataset->num_instances(); i++) // for each instance compare the true class and predicted class {
+    for (int i = 0; i < dataset->num_instances(); i++) { // for each instance compare the true class and predicted class
         int trueClass = dataset->get_instance(i)->get(dataset->num_attributes() - 1)->operator int32();
         int predictedClass = predictions[i];
 
@@ -127,21 +134,38 @@ int main(int argc, char *argv[]) {
     int datasetSizes[2] = {(int)train->num_instances(), (int)test->num_instances()};
 
     // Allocate host memory
-    // float (*h_train_instances)[numAttr] = (float * [numAttr]) malloc(sizeof(float[datasetSizes[0]][numAttr]));
     float *h_train_instances = (float *) malloc(datasetSizes[0] * numAttr * sizeof(float));
-    // float (*h_test_instances)[numAttr] = (float * [numAttr]) malloc(sizeof(float[datasetSizes[1]][numAttr]));
     float *h_test_instances = (float *) malloc(datasetSizes[1] * numAttr * sizeof(float));
     int *h_predictions = (int *) malloc(datasetSizes[1] * sizeof(int));
+    float *h_candidates = (float *) malloc(datasetSizes[1] * k * 2 * sizeof(float));
 
     for (int i = 0; i < datasetSizes[0]; i++) {
-        for (int j = 0; j < numAttr; j++)
-        {
+        for (int j = 0; j < numAttr; j++) {
             h_train_instances[i * numAttr + j] = train->get_instance(i)->get(j)->operator float();
 
             if (i < datasetSizes[1])
                 h_test_instances[i * numAttr + j] = test->get_instance(i)->get(j)->operator float();
         }
     }
+
+    for (int i = 0; i < datasetSizes[1] * 2 * k; i++) {
+        h_candidates[i] = FLT_MAX;
+    }
+
+    // Allocate device memory
+    float *d_train_instances, *d_test_instances;
+    int *d_predictions;
+    float *d_candidates; 
+    int *d_class_counts, *d_dataset_sizes; 
+
+    cudaMalloc(&d_train_instances, datasetSizes[0] * numAttr * sizeof(float));
+    cudaMalloc(&d_test_instances, datasetSizes[1] * numAttr * sizeof(float));
+    cudaMalloc(&d_predictions, datasetSizes[1] * sizeof(int));
+    cudaMalloc(&d_candidates, datasetSizes[1] * k * 2 * sizeof(float));
+    cudaMalloc(&d_class_counts, datasetSizes[1] * numClasses * sizeof(int));
+    cudaMalloc(&d_dataset_sizes, 2 * sizeof(int));
+
+    cudaMemset(d_class_counts, 0, datasetSizes[1] * numClasses * sizeof(int));
 
     float milliseconds = 0;
     cudaEvent_t start, stop;
@@ -150,39 +174,40 @@ int main(int argc, char *argv[]) {
 
     cudaEventRecord(start);
 
-    // Allocate device memory
-    float *d_train_instances, *d_test_instances;
-    int *d_predictions;
-
-    cudaMalloc(&d_train_instances, datasetSizes[0] * numAttr * sizeof(float));
-    cudaMalloc(&d_test_instances, datasetSizes[1] * numAttr * sizeof(float));
-    cudaMalloc(&d_predictions, datasetSizes[1] * sizeof(int));
-
     // Copy host memory to device memory
     cudaMemcpy(d_train_instances, h_train_instances, datasetSizes[0] * numAttr * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_test_instances, h_test_instances, datasetSizes[1] * numAttr * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_predictions, h_predictions, datasetSizes[1] * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_candidates, h_candidates, datasetSizes[1] * k * 2 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dataset_sizes, datasetSizes, 2 * sizeof(int), cudaMemcpyHostToDevice);
 
     // Configure the blocks and grid sizes
     int threadsPerBlock = 64;
     int gridSize = (datasetSizes[1] + threadsPerBlock - 1) / threadsPerBlock;
 
     // Launch the kernel function
-    KNN<<<gridSize, threadsPerBlock>>>(d_train_instances, d_test_instances, d_predictions, k, numAttr, numClasses, datasetSizes);
+    KNN<<<gridSize, threadsPerBlock>>>(d_train_instances, d_test_instances, d_predictions, d_candidates, d_class_counts, d_dataset_sizes, k, numAttr, numClasses);
 
     // Transfer device results to host memory
     cudaMemcpy(h_predictions, d_predictions, datasetSizes[1] * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaError_t cudaError = cudaGetLastError();
+  
+    if(cudaError != cudaSuccess) {
+        fprintf(stderr, "cudaGetLastError() returned %d: %s\n", cudaError, cudaGetErrorString(cudaError));
+        exit(EXIT_FAILURE);
+    }
 
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&milliseconds, start, stop);
 
     // Compute the confusion matrix
-    // int* confusionMatrix = computeConfusionMatrix(predictions, test);
+    int* confusionMatrix = computeConfusionMatrix(h_predictions, test);
     // Calculate the accuracy
-    // float accuracy = computeAccuracy(confusionMatrix, test);
+    float accuracy = computeAccuracy(confusionMatrix, test);
 
-    // printf("The %i-NN classifier for %lu test instances on %lu train instances required %f ms CPU time. Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(), milliseconds, accuracy);
+    printf("The %i-NN classifier for %lu test instances on %lu train instances required %f ms CPU time. Accuracy was %.4f\n", k, datasetSizes[1], datasetSizes[0], milliseconds, accuracy);
 
     // printf("%d, %d, %d, %d", k, datasetSizes[1], threadsPerBlock, gridSize);
 
